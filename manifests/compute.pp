49fdc7dcb1761b5227bd19c66f5339450b3ec3d4
obrestad <eigilo@hig.no>
2015-06-01 12:20:15 +0200

    Added netron and nova to compute.

--- manifests/compute.pp
+++ manifests/compute.pp
@@ -2,4 +2,8 @@ class role::compute {
   # Baseconfiguration. Should be on all hosts.
   include ::profile::baseconfig
   include ::profile::ceph::client
+
+  # Openstack compute
+  include ::profile::openstack::neutronagent
+  include ::profile::openstack::novacompute
 }
