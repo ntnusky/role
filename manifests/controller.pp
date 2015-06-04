1e39eb47078b864bb1e114042ac1eb26073554e1
obrestad <eigilo@hig.no>
2015-06-04 10:09:03 +0200

    Added declaration of a couple of default networks.

--- manifests/controller.pp
+++ manifests/controller.pp
@@ -14,6 +14,7 @@ class role::controller {
   include ::profile::openstack::glance
   include ::profile::openstack::novacontroller
   include ::profile::openstack::neutronserver
+  include ::profile::openstack::neutronnetworks
   
   # Ceph
   include ::profile::ceph::monitor
