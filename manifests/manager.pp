60ff1471163aaddeaec9497d156213e729db469d
Eigil Obrestad <eigil-github@obrestad.org>
2015-06-12 19:08:06 +0200

    Added the NFS server profile to manager.

--- manifests/manager.pp
+++ manifests/manager.pp
@@ -2,4 +2,7 @@ class role::manager {
   # Baseconfiguration. Should be on all hosts.
   include ::profile::baseconfig
   include ::profile::users
+  
+  # NFS server.
+  include ::profile::nfs::server
 }
