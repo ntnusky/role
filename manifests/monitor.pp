22aebc3bcc3e47081806b42b33e3c4197c6eab38
Eigil Obrestad <eigil-github@obrestad.org>
2015-06-22 14:57:56 +0200

    Add the NoRPF module to all roles.

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -2,5 +2,6 @@ class role::monitor {
   # Baseconfiguration. Should be on all hosts.
   include ::profile::baseconfig
   include ::profile::users
+  include ::profile::norpf
   include ::profile::monitoring::elk
 }
