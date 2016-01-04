591d849c9fddefca807bb2e67f0b4f1dae9e30a4
Eigil Obrestad <eigil-github@obrestad.org>
2016-01-04 15:58:27 +0100

    Removed puppetdb. Needs testing before thrown back to production.

--- manifests/manager.pp
+++ manifests/manager.pp
@@ -6,5 +6,5 @@ class role::manager {
   include ::profile::monitoring::logstashforwarder
   include ::profile::managerbackups
   include ::profile::monitoring::icingaclient
-  include ::profile::puppetdb
+  #include ::profile::puppetdb
 }
