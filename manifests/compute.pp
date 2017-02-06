245934e31675aec79227f39925723debff960467
Eigil Obrestad <eigil-git@obrestad.org>
2017-02-06 15:55:23 +0100

    Add compute-related munin-plugins to the compute nodes

--- manifests/compute.pp
+++ manifests/compute.pp
@@ -9,6 +9,7 @@ class role::compute {
   # Openstack compute
   include ::profile::openstack::neutronagent
   include ::profile::openstack::novacompute
+  include ::profile::munin::plugin::compute
 
   # Monitoring
   #include ::profile::monitoring::logstashforwarder
