43e4e8dab5e9bd34d0090dfb43662de63d0e0b87
Eigil Obrestad <eigil-git@obrestad.org>
2017-08-30 14:15:48 +0200

    Added the bootstrap role

---
+++ manifests/bootstrap.pp
@@ -0,0 +1,9 @@
+# This role installs and configures a bootstrap machine, which is able to
+# install the rest of the machines. The bootstrap role is a temporary role, and
+# its services will be delivered by other roles later on in an redundant manner.
+class role::bootstrap {
+  include ::profile::baseconfig
+  include ::profile::baseconfig::users
+
+  include ::profile::serices::tftpserver
+}
