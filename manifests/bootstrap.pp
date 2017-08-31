df93b433f191e2f7581e05cfbcd45d2df6a4db8f
Eigil Obrestad <eigil-git@obrestad.org>
2017-08-31 13:57:12 +0200

    Add the dashboard service

--- manifests/bootstrap.pp
+++ manifests/bootstrap.pp
@@ -6,6 +6,7 @@ class role::bootstrap {
   include ::profile::baseconfig::users
 
   include ::profile::services::apache
+  include ::profile::services::dashboard
   include ::profile::services::dhcp
   include ::profile::services::tftp
 }
