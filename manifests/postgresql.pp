722d80a53c9a2854f4acb60f902951a7500cb5a3
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-25 11:43:31 +0200

    Add a role for the postgresql server

---
+++ manifests/postgresql.pp
@@ -0,0 +1,8 @@
+class role::postgresql {
+  # Baseconfiguration. Should be on all hosts.
+  include ::profile::baseconfig
+  include ::profile::baseconfig::users
+
+  # Include the postgresql profile 
+  include ::profile::services::postgresql
+}
