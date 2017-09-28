994f08f53cfa75d9e7e0841a6b7e7cb3093ea6b2
Eigil Obrestad <eigil-git@obrestad.org>
2017-09-28 13:59:32 +0200

    Seperate role for postgres master and postgres slave

---
+++ manifests/postgresql/master.pp
@@ -0,0 +1,11 @@
+class role::postgresql::master {
+  # Baseconfiguration. Should be on all hosts.
+  include ::profile::baseconfig
+  include ::profile::baseconfig::users
+
+  # Include the postgresql profile 
+  include ::profile::services::postgresql
+
+  # Create databases
+  include ::profile::services::puppetdb::database
+}
--- manifests/postgresql/slave.pp
+++ manifests/postgresql/slave.pp
@@ -1,11 +1,8 @@
-class role::postgresql {
+class role::postgresql::slave {
   # Baseconfiguration. Should be on all hosts.
   include ::profile::baseconfig
   include ::profile::baseconfig::users
 
   # Include the postgresql profile 
   include ::profile::services::postgresql
-
-  # Create databases
-  include ::profile::services::puppetdb::database
 }
