aaa26ced0f69c1054376f8262bdb98d847d4c50f
Eigil Obrestad <eigil-git@obrestad.org>
2017-10-11 11:05:31 +0200

    Added a role for the management loadbalancer

---
+++ manifests/balancer/management.pp
@@ -0,0 +1,9 @@
+# This role installs a loadbalancer for all back-end services.
+class role::balancer::management {
+  include ::profile::baseconfig
+  include ::profile::baseconfig::users
+
+  include ::profile::services::mysql::haproxy::frontend
+  include ::profile::services::keepalived::haproxy::management
+  include ::profile::services::puppetmaster::haproxy::frontend
+}
