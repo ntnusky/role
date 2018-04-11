6a5c785697ad07df6ccdf36a8ddab5efdc045a11
Eigil Obrestad <eigil-git@obrestad.org>
2018-04-11 12:06:17 +0200

    Added a role for glance servers

---
+++ manifests/openstack/glance.pp
@@ -0,0 +1,8 @@
+class role::openstack::glance {
+  # Baseconfiguration. Should be on all hosts.
+  include ::profile::baseconfig
+  include ::profile::baseconfig::users
+
+  # Install the glance service
+  include ::profile::openstack::glance
+}
