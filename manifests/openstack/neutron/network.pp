2b74cef6cca3a7fd771678aeb4b00cdf6e144ba4
Eigil Obrestad <eigil-git@obrestad.org>
2018-05-28 14:55:31 +0200

    Added a role for the neutron network node

---
+++ manifests/openstack/neutron/network.pp
@@ -0,0 +1,9 @@
+# This class installs a neutron network node
+class role::openstack::neutron::network {
+  # Baseconfiguration. Should be on all hosts.
+  include ::profile::baseconfig
+  include ::profile::baseconfig::users
+
+  # Install the neutron network node
+  include ::profile::openstack::neutron::network
+}
