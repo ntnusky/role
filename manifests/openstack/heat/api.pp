117f469cb8adf2fcecf2e96525e146ded50a26a7
Eigil Obrestad <eigil-git@obrestad.org>
2018-05-29 10:51:54 +0200

    Add heat roles to support dedicated heat nodes

---
+++ manifests/openstack/heat/api.pp
@@ -0,0 +1,9 @@
+# Server-role for the heat API servers
+class role::openstack::heat::api {
+  # Baseconfiguration. Should be on all hosts.
+  include ::profile::baseconfig
+  include ::profile::baseconfig::users
+
+  # Install the heat API
+  include ::profile::openstack::heat::api
+}
---
+++ manifests/openstack/heat/engine.pp
@@ -0,0 +1,9 @@
+# Server-role for the heat engine servers
+class role::openstack::heat::engine {
+  # Baseconfiguration. Should be on all hosts.
+  include ::profile::baseconfig
+  include ::profile::baseconfig::users
+
+  # Install the heat engine
+  include ::profile::openstack::heat::engine
+}
