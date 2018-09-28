21497d02379e0604f3f65bdcfcf3e538290590d9
Lars Erik Pedersen <pedersen.larserik@gmail.com>
2018-09-28 10:26:01 +0200

    Add openstack clients to klumpen

--- manifests/klumpen.pp
+++ manifests/klumpen.pp
@@ -1,5 +1,7 @@
+# Role for bulk storage box called klumpen
 class role::klumpen {
   include ::profile::baseconfig
   include ::profile::baseconfig::users
   include ::profile::ceph::client
+  include ::ntnuopenstack::clients
 }
