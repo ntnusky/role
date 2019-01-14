ba5a662834d4d93ceee00cdccd677ba1add499fe
Eigil Obrestad <eigil-git@obrestad.org>
2019-01-14 17:10:45 +0100

    Naming

--- manifests/openstack/swift.pp
+++ manifests/openstack/swift.pp
@@ -1,4 +1,6 @@
-class role::openstack::keystone {
+# Openstack swift role. Installs a ceph rados gateway, and registers it in
+# keystone as a swift node.
+class role::openstack::swift {
   # Baseconfiguration. Should be on all hosts.
   include ::profile::baseconfig
   include ::profile::baseconfig::users
