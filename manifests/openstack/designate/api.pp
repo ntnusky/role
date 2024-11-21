d7b328e8ee8021a1b75913db0c1c414e78ce6bf9
Felix Albrigtsen <felix.albrigtsen@ntnu.no>
2024-11-21 16:20:58 +0100

    Update designate for multiregion

--- manifests/openstack/designate/api.pp
+++ manifests/openstack/designate/api.pp
@@ -3,6 +3,17 @@ class role::openstack::designate::api {
   include ::profile::baseconfig
   include ::profile::baseconfig::users
 
-  # Install the openstack designate server
-  include ::ntnuopenstack::designate::api
+  $regionless = lookup('profile::region::missing::ok', {
+    'default_value' => false,
+    'value_type'    => Boolean,
+  })
+
+  if($regionless or ($::facts['openstack'] and $::facts['openstack']['region'])) {
+    # Install the openstack designate server
+    include ::ntnuopenstack::designate::api
+  } else {
+    notify { 'Base-Only':
+      message => 'Only role::base applied due to missing region fact',
+    }
+  }
 }
--- manifests/openstack/designate/ns.pp
+++ manifests/openstack/designate/ns.pp
@@ -3,6 +3,17 @@ class role::openstack::designate::ns {
   include ::profile::baseconfig
   include ::profile::baseconfig::users
 
-  # Install the nameserver for designate
-  include ::ntnuopenstack::designate::ns
+  $regionless = lookup('profile::region::missing::ok', {
+    'default_value' => false,
+    'value_type'    => Boolean,
+  })
+
+  if($regionless or ($::facts['openstack'] and $::facts['openstack']['region'])) {
+    # Install the nameserver for designate
+    include ::ntnuopenstack::designate::ns
+  } else {
+    notify { 'Base-Only':
+      message => 'Only role::base applied due to missing region fact',
+    }
+  }
 }
