f1e0ab3755ef16c58d95274b1446ac1265fdf03a
steinaros <steinar.o.sjoholt@ntnu.no>
2025-01-09 10:38:32 +0100

    Include Zookeeper profile

--- manifests/zookeeper.pp
+++ manifests/zookeeper.pp
@@ -2,4 +2,6 @@ class role::zookeeper {
   # Baseconfiguration. Should be on all hosts.
   include ::profile::baseconfig
   include ::profile::baseconfig::users
+  # Include Zookeeper profile
+  include ::profile::services::zookeeper
 }
